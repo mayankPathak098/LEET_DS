ebf3ef81eb3acf5fd483a32e6b3648144c61b388
mayankPathak098 <166496280+mayankPathak098@users.noreply.github.com>
2026-03-09 18:37:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (75.68%) - LeetSync

--- 171-excel-sheet-column-number/excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/excel-sheet-column-number.cpp
@@ -3,8 +3,7 @@ public:
 //result = result × 26 + (character − 'A' + 1)
     int titleToNumber(string columnTitle) {
         int result=0;
-        int n=columnTitle.size();
-        for(int i=0;i<n;i++){
+        for(int i=0;i<columnTitle.size();i++){
             result=result*26 +(columnTitle[i]-'A'+1);
         }
         return result;
