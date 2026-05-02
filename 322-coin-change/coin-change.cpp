dece01b5b8ad77ae269872e9ac69da083a6445fa
mayankPathak098 <166496280+mayankPathak098@users.noreply.github.com>
2026-05-02 19:22:32 +0530

    Time: 44 ms (34.52%) | Memory: 49.4 MB (26.97%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-    int mincoin(vector<int>& coins,vector<int>&dp, int amount){
-        if(amount==0) return 0;
-        else if(amount<0) return INT_MAX-1;
-        if(dp[amount]!=-1) return dp[amount];
+    int knapsack(int amount,vector<int>& coins,int idx,vector<vector<int>>&dp){
+       if(idx==0){
+        if(amount%coins[0]==0) return amount/coins[0];
+        return INT_MAX-1;
+       }
+        if(dp[idx][amount]!=-1) return dp[idx][amount];
 
-        int mini=INT_MAX-1;
+       int nottake=0+knapsack(amount,coins,idx-1,dp);
+       int take=INT_MAX-1;
+       if(coins[idx]<=amount) take=1+knapsack(amount-coins[idx],coins,idx,dp);
 
-        for(int val:coins){
-            int res=mincoin(coins,dp,amount-val);
-             if (res != INT_MAX - 1) {
-                mini = min(mini, res + 1);
-            }
-        }
-
-        return dp[amount]=mini;
-
-
+        return dp[idx][amount]=min(take,nottake);
     }
     int coinChange(vector<int>& coins, int amount) {
-        
-        vector<int>dp(amount+1,-1);
-        int ans = mincoin(coins, dp, amount);
-        if(ans >= INT_MAX - 1) return -1;
-        return ans;
-        
+        int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+
+        int ans=knapsack(amount,coins,n-1,dp);
+
+        return ans>=INT_MAX-1?-1:ans; 
     }
 };
