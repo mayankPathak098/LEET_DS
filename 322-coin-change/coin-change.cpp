3b0433f380a0dccd2401fd8014088e4361bef549
mayankPathak098 <166496280+mayankPathak098@users.noreply.github.com>
2026-05-05 20:28:17 +0530

    Time: 42 ms (38.67%) | Memory: 49.5 MB (22.32%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    int knapsack(int amount,vector<int>& coins,int idx,vector<vector<int>>&dp){
-       if(idx==0){
-        if(amount%coins[0]==0) return amount/coins[0];
-        return INT_MAX-1;
-       }
-        if(dp[idx][amount]!=-1) return dp[idx][amount];
+    int knapsack(int target,vector<int>& coins,int idx,vector<vector<int>>&dp){
+        if(idx==0){
+            if(target%coins[0]==0) return target/coins[0];
+            return INT_MAX-1;
+        }
+        if(dp[idx][target]!=-1) return dp[idx][target];
 
-       int nottake=0+knapsack(amount,coins,idx-1,dp);
-       int take=INT_MAX-1;
-       if(coins[idx]<=amount) take=1+knapsack(amount-coins[idx],coins,idx,dp);
+        int nottake=knapsack(target,coins,idx-1,dp);
+        int take=INT_MAX-1;
+        if(coins[idx]<=target) take=1+knapsack(target-coins[idx],coins,idx,dp);
 
-        return dp[idx][amount]=min(take,nottake);
+        return dp[idx][target]=min(take,nottake);
+
+
     }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
+        
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
 
         int ans=knapsack(amount,coins,n-1,dp);
