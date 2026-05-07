cb77f841279962fd6d0490183c2f2df7238836bd
mayankPathak098 <166496280+mayankPathak098@users.noreply.github.com>
2026-05-07 15:33:58 +0530

    Time: 375 ms (45.50%) | Memory: 46.9 MB (56.36%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -3,27 +3,29 @@ public:
     
     int minCostConnectPoints(vector<vector<int>>& points) {
         int v=points.size();
+        vector<vector<pair<int,int>>>adj(v);
         
         int cost=0;
+
         priority_queue< pair<int,int>,vector<pair<int,int>>,greater<>> pq;
         pq.push({0,0});
-        
         vector<int>vis(v,0);
         while(!pq.empty()){
-            auto it=pq.top();
+            auto[weight,vertex]=pq.top();
             pq.pop();
-            int value=it.first;
-            int u=it.second;
-            if(vis[u]==0){
-                vis[u]=1;
-                cost+=value;
-                for(int i = 0; i < v; i++) {
-                if(vis[i] == 0) {
-                    int dist = abs(points[u][0] - points[i][0]) + abs(points[u][1] - points[i][1]);
-                    pq.push({dist, i});
+
+            if(vis[vertex]==1) continue;
+            vis[vertex]=1;
+
+            cost+=weight;
+            for(int i=0;i<v;i++){
+                if(vis[i]==0) {
+                    int dist=abs(points[vertex][0]-points[i][0])+abs(points[vertex][1]-points[i][1]);
+                    pq.push({dist,i});
                 }
             }
-        }
+        }    
+        return cost;
+
     }
-    return cost;  }
 };
